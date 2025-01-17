81821a2ecb0b1ed42e56b84b91c404d23b432eb3
FernandoCanabarroAhnert <ahnertfernando499@gmail.com>
2025-01-17 18:15:27 -0300

    teste sendgrid 9

--- src/main/java/com/fernandocanabarro/oak_desafio/config/SendGridConfig.java
+++ src/main/java/com/fernandocanabarro/oak_desafio/config/SendGridConfig.java
@@ -4,16 +4,15 @@ import com.sendgrid.SendGrid;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 public class SendGridConfig {
 
-    // @Bean
-    // public SendGrid sendGrid() {
-    //     // Use a chave de API configurada como variável de ambiente
-    //     String apiKey = System.getenv("SENDGRID_API_KEY");
-    //     if (apiKey == null || apiKey.isBlank()) {
-    //         throw new IllegalStateException("SENDGRID_API_KEY não configurado");
-    //     }
-    //     return new SendGrid(apiKey);
-    // }
+    @Bean
+    public SendGrid sendGrid() {
+        String apiKey = System.getenv("SENDGRID_API_KEY");
+        if (apiKey == null || apiKey.isBlank()) {
+            throw new IllegalStateException("SENDGRID_API_KEY não configurado");
+        }
+        return new SendGrid(apiKey);
+    }
 }
